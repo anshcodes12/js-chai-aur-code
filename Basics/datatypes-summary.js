2545725363d07032bd54f612ddac2efec39f81ca
Ansh Agnihotri <mailforwork.ansh1@gmail.com>
2025-06-23 20:03:27 +0530

    datatypes summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,49 @@
+//there are two types of data types
+// primitive and non-primitive
+
+//primitive:types
+/*7 Types: 
+1.String
+2.Number
+3.Boolean
+4.Null
+5.Undefined
+6.Symbol
+7.BigInt
+*/
+
+// ------------------------------------------------------
+
+//Non-primitive(reference type)
+/*
+-Array
+-Objects 
+-Functions
+
+Note: FOR MASTERING JAVA SCRIPT MASTER 'OBJECTS' AND 'WEB/BROWSER EVENTS'.
+*/ 
+const heros = ["shaktimaan", "naagraj", "doga"]; //example of array
+
+//objects example (in curly brackets)
+//values in an object are feeded as key value function
+//can also define as a variable (myObj, here)
+
+let myObj = {
+  name: "ansh",
+  age: 21,
+}
+
+//function example
+//saved in a variable
+const myFunction = function () {
+  console.log("Hello world")
+}
+
+//java script doesn't require to tell the type of language
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+//here we don't needed to define the type but it got detected automatically
