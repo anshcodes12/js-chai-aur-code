bc6c7a58a59597a9254d89d55fb234d57baa339d
Ansh Agnihotri <mailforwork.ansh1@gmail.com>
2025-08-02 00:21:57 +0530

    functions commit

---
+++ 03_Basics/12_functions.js
@@ -0,0 +1,93 @@
+//packaging a piece of code to use it numerous places but writing once
+
+function sayMyName() {
+console.log("My name is Ansh");
+}
+
+// function addTwoNumbers(number1, number2) // parameters inside bracket
+// {
+// console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2) // parameters inside bracket
+{
+let result = number1 + number2
+return result
+console.log("ansh"); //nothing gets printed after return statement taking it above return will print
+
+return number1 + number2
+
+}
+
+
+function sayMyName(){
+  console.log("H");
+  console.log("I");
+  console.log("T");
+  console.log("E");
+  console.log("S");
+  console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+  // let result = number1 + number2
+  // return result
+  return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+  if(!username){
+      console.log("PLease enter a username");
+      return
+  }
+  return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+  username: "hitesh",
+  prices: 199
+}
+
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+  username: "sam",
+  price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+ 
+
